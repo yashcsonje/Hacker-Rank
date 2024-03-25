3505519bc671cc8ab1eafd419eca0f7c11309cd3
yashcsonje <162028752+yashcsonje@users.noreply.github.com>
2024-03-25 11:11:56 +0530

    Add files via upload

---
+++ Python/Arithmetic_Operators.py
@@ -0,0 +1,13 @@
+#Python Program: Arithmetic Operations on Two Integers
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+sum=a+b
+print(sum)
+
+sub=a-b
+print(sub)
+
+product=a*b
+print(product)
