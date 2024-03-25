c05c28e0d48fbbc576795c42efe63f9c2381ace6
yashcsonje <162028752+yashcsonje@users.noreply.github.com>
2024-03-25 11:14:45 +0530

    Add files via upload

---
+++ Python/Division.py
@@ -0,0 +1,11 @@
+#Python Program: Integer and Float Division of Two Numbers
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+def floardiv(a, b):
+    return a//b
+def div(a, b):
+    return a/b
+print(floardiv(a, b))
+print(div(a, b))
